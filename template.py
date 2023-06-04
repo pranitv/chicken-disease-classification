cab837ec70c3067f9fc811e4182fcebcf1c5d4d8
pranitv <pranitvithalkar7@gmail.com>
2023-06-04 11:27:36 +0530

    added setup.py

--- template.py
+++ template.py
@@ -20,8 +20,9 @@ list_of_files = [
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
-    "setup.py"
-    "research/trials.ipynb"
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
